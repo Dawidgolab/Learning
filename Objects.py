0093dbac9f2021378057e044e6375acd7663f5b1
Dawid Gołąb <dawidg122@interia.pl>
2023-09-21 09:23:11 +0200

    Shallow and deep copying we use shallow and deep copying and instead of them we can also use e.g. for list 'mylist' a. list(mylist) b. mylist[:]

--- Objects.py
+++ Objects.py
@@ -8,7 +8,7 @@ we can use with the object
 
 #immutable(niezmienna) object
 # (bool, int, float, tuple, str)
-print("""#####Immutable######: 
+print("""!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#####Immutable######: 
 immerse objects guarantee us that under a will be 4,
 even if 7 is assigned to the value of b ####
 !!!!!!!Change doesn't occurs on the original !!!!!!!!""")
@@ -20,8 +20,10 @@ print(b,"\n")
 
 
 #mutable(zmienna)
-print("""######Function id returns us the address of objects#####
-!!!!!!!Change occurs on the original !!!!!!!!""")
+print("""!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!####Mutable#####
+Function id returns us the address of objects
+Change occurs on the original !!!!!!!!""")
+
 listSample = [1,4,534,23]
 print("1 id of 1 list :",id(listSample))
 listSample2 = listSample
@@ -41,6 +43,8 @@ def add(c, amount = 1):
 print("\n")
 
 
+
+
 #List with id
 print("####List with id######")
 def append_element_to_list(lista,element):
@@ -51,3 +55,44 @@ def append_element_to_list(lista,element):
 print("Main list where we dont use the function:",id(listSample))
 append_element_to_list(listSample,5)
 print(listSample)
+print()
+
+
+
+
+
+#Unwanted changes in funtion(shallow and deep copying)
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!shallow and deep copying\n")
+
+print("Shallow copying")
+def evil_function(toBeDestroyedList):
+    toBeDestroyedList[0] = 4
+    print(f"Copied list : {toBeDestroyedList}")
+
+mylist = [1,4,2,1,53,3]
+
+# shallow copying(it used to immutable)
+evil_function(mylist.copy())
+
+print(f"Non Copied list: {mylist}\n\n")
+
+#deep
+print("Deep copying")
+
+import copy
+def evil_function2(toBeDestroyedList2):
+    toBeDestroyedList2[0][0] = 4
+    print(f"Copied list with deep copying : {toBeDestroyedList2}")
+
+mylist2 = [[1,2],[3,4],[5,6]]
+
+# deep copying(it used to mutable because shallow copying doesnt
+# work because we change everything but not only one list)
+evil_function2(copy.deepcopy(mylist2))
+
+print(f"Non Copied list: {mylist2}")
+
+
+# instead of copy we can use :
+# 1. list(mylist) e.g.
+# 2. mylist[:] (mylist[0:3] - wycinanie elementów z listy)
