9a83971b2790ba1a4fa94effecfe18311eea8666
Dawid Gołąb <dawidg122@interia.pl>
2023-10-03 18:12:59 +0200

    Collections with Counter function

--- OtherFunctions.py
+++ OtherFunctions.py
@@ -1,3 +1,13 @@
 # split - we can separate two words
 full_name = "Marek Kowalski"
-print(full_name .split()[0])
+print(full_name .split()[0],"\n")
+
+#collections with Counter - count the elements and create dictionary
+from collections import Counter
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+def evenNumbers(list):
+    return [number for number in list if number % 2 == 0]
+
+print(Counter(evenNumbers(list)))
