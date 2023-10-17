7403e13801973f6c434deaaf18d582c823608011
Dawid Gołąb <dawidg122@interia.pl>
2023-10-17 11:00:36 +0200

    Small changes in the chests! ! I had a problem with a chests because i couldn't display the 'Color' because there was a tuple in the dictionary so i've removed the comma after each of the boxes and everything now works

--- Tasks.py
+++ Tasks.py
@@ -39,9 +39,9 @@ myAccount = 0
 Prize = ["Congratulations! You've found a chest!", "Unfortunately, you found nothing."]
 
 
-Chest1 = {'Color':'Green', 'Points': 1000},
-Chest2 = {'Color':'Orange', 'Points': 4000},
-Chest3 = {'Color':'Purple' ,'Points': 9000},
+Chest1 = {'Color':'Green', 'Points': 1000}
+Chest2 = {'Color':'Orange', 'Points': 4000}
+Chest3 = {'Color':'Purple' ,'Points': 9000}
 Chest4 = {'Color':'Gold', 'Points': 16000}
 Chests = [Chest1,Chest2,Chest3,Chest4]
 
@@ -51,7 +51,7 @@ print("You have only 5 moves left. Let's see how much treasure you can uncover!"
 
 gamerChoice = 1
 
-while gamerChoice <= 10:
+while gamerChoice <= 5:
     print(f"Move {gamerChoice}:")
     step = input("Do you wish to move forward? (Type 'yes' or 'no'):  \n")
     gamerChoice += 1
@@ -64,16 +64,16 @@ while gamerChoice <= 10:
         print(stepYesOrNo)
 
         if stepYesOrNo == Prize[0]:
-            percentageOfTheBox = random.choices(Chests,[75,20,4,1],k = 100)
+            percentageOfTheBox = random.choices(Chests,[75,20,4,1],k = 100 )
             kindOfTheBox = random.choice(percentageOfTheBox)
-            print("You've discovered :", kindOfTheBox)
+            print("You've discovered :", kindOfTheBox['Color'])
 
 
 
 #Finish the program
     elif step == 'no':
-        break
         print("Goodbye! Thanks for playing.")
+        break
 
 #What if we use the other options
     else:
