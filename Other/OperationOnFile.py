c8ce224fcf6ca241090e63e3d4ee071c772087c0
Dawid Gołąb <dawidg122@interia.pl>
2023-10-23 13:32:29 +0200

    theory part-one

--- Other/OperationOnFile.py
+++ Other/OperationOnFile.py
@@ -1,3 +1,13 @@
-with open('sowpods.txt','r') as file:
-    content = file.read()
-    print(content)
+#Type of the operations on file:
+'''
+- opening
+- writing/reading
+- closing
+'''
+# How to open the files:
+'''
+1. r - Reading - default
+2. w - Writing - if the file already exists, it will delete it and save a new one ,
+ but if it does not exist, a new one will be created
+3. a - Append 
+'''
