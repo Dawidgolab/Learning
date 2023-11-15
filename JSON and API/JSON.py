8a84cc5dd54bf94f0d877bf50e4d866c0ffbf226
Dawid Gołąb <dawidg122@interia.pl>
2023-11-15 12:23:13 +0100

    This is a base that we will use

---
+++ JSON and API/JSON.py
@@ -0,0 +1,36 @@
+"""
+
+JSON - JSON stands for JavaScript Object Notation. It's a lightweight data-interchange format that is easy for humans to read and write,
+
+"""
+
+film = {
+    "title" : "Ale ja nie będę tego robił!",
+    "release_year" : 1969,
+    "won_oscar" : True,
+    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
+    "budget" : None,
+    "credits" : {
+        "director" : "Arkadiusz Włodarczyk",
+        "writer" : "Alan Burger",
+        "animator" : "Anime Animatrix"
+    }
+}
+
+"""
+{  
+   "title":"Ale ja nie będę tego robił!",
+   "release_year":1969,
+   "won_oscar":true,
+   "actors":[  
+      "Arkadiusz Włodarczyk",
+      "Wiolleta Włodarczyk"
+   ],
+   "budget":null,
+   "credits":{  
+      "director":"Arkadiusz Włodarczyk",
+      "writer":"Alan Burger",
+      "animator":"Anime Animatrix"
+   }
+}
+"""
