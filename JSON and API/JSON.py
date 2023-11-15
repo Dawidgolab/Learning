9ac3a86a1a4636f6f8c9879a8c87def79ec83de4
Dawid Gołąb <dawidg122@interia.pl>
2023-11-15 12:41:47 +0100

    How to download the JSON file

--- JSON and API/JSON.py
+++ JSON and API/JSON.py
@@ -2,21 +2,38 @@
 
 JSON - JSON stands for JavaScript Object Notation. It's a lightweight data-interchange format that is easy for humans to read and write,
 
+json.dumps(data) - save data to as a string json
+json.dump(data,nameOdFile,ensure_ascii=False) - save data to the file as jason
 """
 
+# How to download the JSON file????
+
+import json #We import the json
+
 film = {
-    "title" : "Ale ja nie będę tego robił!",
+    "title" : "Zrobie to zawsze!",
     "release_year" : 1969,
     "won_oscar" : True,
-    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
+    "actors": ("Dawid Gołąb", "Alicja Gołąb"),
     "budget" : None,
     "credits" : {
-        "director" : "Arkadiusz Włodarczyk",
-        "writer" : "Alan Burger",
-        "animator" : "Anime Animatrix"
+        "director" : "Dawid Gołąb",
+        "writer" : "Barry Cent",
+        "animator" : "Calvin Matrix"
     }
 }
 
+# dumps (String)
+encodedFilm = json.dumps(film,ensure_ascii=False)
+print(encodedFilm)
+
+
+# dump (File)
+
+with open("sample.json",'w',encoding="UTF-8") as file:
+    json.dump(film,file,ensure_ascii=False)
+
+###############################################################################
 """
 {  
    "title":"Ale ja nie będę tego robił!",
