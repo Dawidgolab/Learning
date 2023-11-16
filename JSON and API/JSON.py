fb00326eeb1a36a1e7e9cb50c7d021fdbc9a1a88
Dawid Gołąb <dawidg122@interia.pl>
2023-11-16 15:51:27 +0100

    text formatting

--- JSON and API/JSON.py
+++ JSON and API/JSON.py
@@ -38,6 +38,7 @@ with open("sample.json",'w',encoding="UTF-8") as file:
 # 1. json.loads(jsonstring) - store jsonstring for data of the phyton type
 # 2. json.load(filePointer) - loaded json from file and return as a result of the method data of the phyton type
 
+'''
 # 1
 encodedRetrievedMovie = '{"title": "Zrobie to zawsze!", "release_year": 1969, "won_oscar": true, "actors": ["Dawid Gołąb", "Alicja Gołąb"], "budget": null, "credits": {"director": "Dawid Gołąb", "writer": "Barry Cent", "animator" : "Calvin Matrix"}}'
 decodedMovie = json.loads(encodedRetrievedMovie)
@@ -48,3 +49,22 @@ print(decodedMovie)
 with open("sample.json",encoding="UTF-8") as file:
     result = json.load(file) # we changed the json format to phyton format
     print(result)
+'''
+####################################################################################
+# text format
+# indent = 4
+# sort_keys = True - alphabetical sorting
+
+# indent = 4
+encodedFilm1 = json.dumps(film,ensure_ascii=False,indent=4)
+print(encodedFilm1)
+print("########################################")
+
+# sort_keys = True
+encodedFilm2 = json.dumps(film,ensure_ascii=False,indent=4,sort_keys=True)
+print(encodedFilm2)
+
+# sorting and formating text into the file
+with open("sample.json","w",encoding="UTF-8") as file:
+    json.dump(film,file,ensure_ascii=False,indent=4,sort_keys=True)
+
