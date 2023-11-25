1162ca92489822505e682ea8c3e97ee3b1ac063d
Dawidgolab <dawidg122@interia.pl>
2023-11-25 09:17:46 +0100

    Importing requests with the sample of the use

--- JSON-pip-PyPi-API/pip-PyPi.py
+++ JSON-pip-PyPi-API/pip-PyPi.py
@@ -3,6 +3,8 @@ pip - pip installs packeges
 Pypi - Phyton Packege index
 To download the library reguests we open the pypi.org
 """
-
-
 import requests
+
+interia = requests.get("https://www.interia.pl/")
+print(interia.status_code)
+
