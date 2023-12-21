7eee9a87001286ddef582b150f2ff10fffdd59b4
Dawidgolab <dawidg122@interia.pl>
2023-12-21 14:52:07 +0100

    Correct added the image and using the PIL(Image)

--- TasksAndGames/Image.py
+++ TasksAndGames/Image.py
@@ -1,1 +1,9 @@
 # Load image width/height
+from PIL import Image
+
+pathToImage = "k.jpg" # You new to add your own path to the image you want to display
+newImage = Image.open(pathToImage)
+print(f"Width: {newImage.width}")
+print(f"Height: {newImage.height}")
+
+#print(newImage.size)
