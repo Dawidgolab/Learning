0c0e05323f978d7e2357a2523b832585d6967d34
Dawidgolab <dawidg122@interia.pl>
2023-12-27 15:28:02 +0100

    Two saples , what API actually is?

--- JSON-pip-PyPi-API/API.py
+++ JSON-pip-PyPi-API/API.py
@@ -1,6 +1,19 @@
 """
 Public API - application programming interface
 
-Inter/face
+* Two samples of API:
+
+Automatic online text translation:
+When you use an online text translation application such as Google Translate,
+the application uses an API to send the text you want to translate to Google's server.
+There, the text is processed and translated, and then a reply with the translated text
+is sent back to the app for you to see.
+
+Weather on your phone:
+Weather apps on the phone often use weather service APIs. When you open the app,
+it sends a query to the server, asking for current weather data for your location.
+The server processes the query and returns a response with the weather forecast,
+which the app displays on your phone.
 
 """
+
