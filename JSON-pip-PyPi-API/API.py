8bccfb0d17223c4f02854f5b9c5e8975a4111087
Dawidgolab <dawidg122@interia.pl>
2024-01-01 18:33:45 +0100

    task 1 -> download json from stackoverflow by means of API from stackexchange

--- JSON-pip-PyPi-API/API.py
+++ JSON-pip-PyPi-API/API.py
@@ -17,3 +17,21 @@ which the app displays on your phone.
 
 """
 
+# task 1 -> download json from stackoverflow by means of API from stackexchange
+
+import requests
+import json
+
+params = {
+    "site": "stackoverflow"
+}
+
+r = requests.get("https://api.stackexchange.com/2.3/questions/", params)
+
+
+try:
+    questions = r.json()
+except json.decoder.JSONDecodeError:
+    print("Invalid format!!!")
+else:
+    print(questions)
