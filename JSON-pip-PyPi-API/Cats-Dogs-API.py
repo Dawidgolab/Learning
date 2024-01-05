983e00cbb66678eecf63f153cc874e56fabb8c01
Dawidgolab <dawidg122@interia.pl>
2024-01-05 12:15:34 +0100

    Solution of the task

--- JSON-pip-PyPi-API/Cats-Dogs-API.py
+++ JSON-pip-PyPi-API/Cats-Dogs-API.py
@@ -4,25 +4,52 @@
 """
 
 import requests
-import webbrowser
 import json
 from pprint import pprint
 
-params = {
-    "animal_type" : "dog",
-    "amount" : 3
-}
+def param_cat():
+    params = {
+    "animal_type" : "cat",
+    "amount" : 5
+    }
+    return params
+
+def param_dog():
+    params = {
+        "animal_type" : "dog",
+        "amount" : 5
+    }
+    return params
+
+def url_json(param):
+    r = requests.get('https://cat-fact.herokuapp.com/facts/random',param)
+    try:
+        facts = r.json()
+    except json.decoder.JSONDecodeError:
+        print("Invalid format!!!")
+    else:
+        for i, fact in enumerate(facts, start=1):
+            print(f"{i}. {fact['text']}")
 
 
 
-r = requests.get('https://cat-fact.herokuapp.com/facts/random',params)
 
-try:
-    facts = r.json()
-except json.decoder.JSONDecodeError:
-    print("Invalid format!!!")
-else:
-    for fact in facts:
-        pprint(fact["text"])
+while True:
+    choice = int(input("Select 5 fact about : 1 - dogs or 2 - cats: "))
+#dogs
+    if choice == 1:
+        url_json(param_dog())
+        break
+#cats
+    elif choice == 2:
+        url_json(param_cat())
+        break
+#The next attempt
+    else:
+        print("Try again!!!")
+        continue
+
+
+
 
 
