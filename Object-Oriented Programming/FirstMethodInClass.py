0ed5aa642bb09bdecd75b3f2a4162e6dc58f925f
Dawidgolab <dawidg122@interia.pl>
2024-02-27 19:11:41 +0100

    Changes:
    -> Rename the 'Exmaple file '
    -> extended the first object in new file

---
+++ Object-Oriented Programming/FirstMethodInClass.py
@@ -0,0 +1,22 @@
+''' 'self' in python but 'this' in another languages '''
+
+
+class User:
+    name = ""
+    age = 0
+    name = "NULL"
+
+    def print_age(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+
+user1 = User()
+user2 = User()
+
+user1.name = "Marek"
+user1.age = 25
+user1.print_age()
+
+user2.name = "Darek"
+user2.age = 20
+user2.print_age()
