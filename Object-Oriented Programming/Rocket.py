6c3af568b5bcbdf084690d4dd4e2988223af5cab
Dawidgolab <dawidg122@interia.pl>
2024-03-01 14:26:03 +0100

    Extended Rocket program

--- Object-Oriented Programming/Rocket.py
+++ Object-Oriented Programming/Rocket.py
@@ -2,20 +2,27 @@
 from random import randint
 
 class Rocket:
+
     def __init__(self):
         self.altitude = 0 #start altitude
+        self.speed = randint(0,4)
     
     def move_up(self):
-        self.altitude += 1 # move altitude
+        self.altitude += self.speed # move altitude
 
 
 
 rockets = [Rocket() for _ in range(5)] # these are (from 0 to 4) objects 
 
+print("Our 10 indexes(from 0 to 4)!!!")
 for _ in range(10):
     rocketIndexToMove = randint(0,4) #random rocket
+    print(rocketIndexToMove)
     rockets[rocketIndexToMove].move_up() # random rocket move up
 
+print("------------------------------------")
+
+print("Our 5 rockets after random(0,4) moves")
 for rocket in rockets:
     print(rocket.altitude)
 
