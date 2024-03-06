1989bcb21153f1cb6ed26f0abe38f7644cd7b700
Dawidgolab <dawidg122@interia.pl>
2024-03-06 16:15:09 +0100

    Project rocket Developing

--- Object-Oriented Programming/RocketTask/Rocket.py
+++ Object-Oriented Programming/RocketTask/Rocket.py
@@ -7,18 +7,22 @@ class Rocket:
         self.altitude = 0 #start altitude
         self.speed = randint(0,4)
        
-        
     def move_up(self):
         self.altitude += self.speed # move altitude
-    
 
     def __str__(self): #function which visualize the object instead of ..at 0x... we can see the string
         return f"Rocket's current altitude: {self.altitude}"
 
 
 class RocketBoard:
-    pass
+    def __init__(self,amountOfRockets=5):
+        self.rockets = [Rocket(randint(1,6)) for _ in range(amountOfRockets)] # these are (from 0 to 4) objects 
 
+        for _ in range(10):
+            rocketIndexToMove = randint(0,len(self.rockets) - 1 ) #random rocket
+            self.rockets[rocketIndexToMove].move_up() # random rocket move up
 
-
-
+        for value ,rocket in enumerate(self.rockets,start=1):
+            print(value,rocket)
+        
+        print("---------------------------------------------")
--- Object-Oriented Programming/RocketTask/myscript.py
+++ Object-Oriented Programming/RocketTask/myscript.py
@@ -1,12 +1,6 @@
-from random import randint
-from Rocket import Rocket,RocketBoard
+from Rocket import RocketBoard
 
-rockets = [Rocket(randint(1,6)) for _ in range(5)] # these are (from 0 to 4) objects 
+board = RocketBoard(2)
 
-for _ in range(10):
-    rocketIndexToMove = randint(0,4) #random rocket
-    rockets[rocketIndexToMove].move_up() # random rocket move up
 
-print("Our 5 rockets after random(0,4) moves:")
-for value ,rocket in enumerate(rockets,start=1):
-    print(value,rocket)
+print(board.rockets[0].altitude)
