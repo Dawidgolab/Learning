8eb4681aa146a92b1a432e5740975545efc46c84
Dawidgolab <dawidg122@interia.pl>
2024-03-13 18:37:02 +0100

    get distance between two points

--- Object-Oriented Programming/RocketTask/Rocket.py
+++ Object-Oriented Programming/RocketTask/Rocket.py
@@ -1,5 +1,6 @@
 # Rocket task
 from random import randint
+from math import sqrt
 
 class Rocket:
 
@@ -8,13 +9,19 @@ class Rocket:
     def __init__(self,speed=1):
         self.altitude = 0 #start altitude
         self.speed = randint(0,4)
+        self.x = 0
+        # make patrol function
        
     def move_up(self):
         self.altitude += self.speed # move altitude
 
     def __str__(self): #function which visualize the object instead of ..at 0x... we can see the string
         return f"Rocket's current altitude: {self.altitude}"
-
+    
+    def get_distance(self,rocket):
+        ab = (rocket.altitude - self.altitude) ** 2
+        bc = (rocket.altitude - self.x) ** 2
+        return sqrt(ab + bc)
 
 class RocketBoard:
     def __init__(self,amountOfRockets=5):
--- Object-Oriented Programming/RocketTask/myscript.py
+++ Object-Oriented Programming/RocketTask/myscript.py
@@ -1,7 +1,14 @@
 from Rocket import RocketBoard
 
-board = RocketBoard(10)
+board = RocketBoard(2)
 
+print(board)
+print(board[0].get_distance(board[1]))
+
+
+'''
 board[0] = 60
-
 print(board[0])
+'''
+
+
