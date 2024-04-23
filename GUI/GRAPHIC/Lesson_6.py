a0f42654ff0b48aa7dcf7776992f2c83afb80456
Dawidgolab <dawidg122@interia.pl>
2024-04-23 17:24:02 +0200

    root - blind - to open the function from the event

---
+++ GUI/GRAPHIC/Lesson_6.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from center import center_window
+from tkinter import messagebox
+
+
+root = Tk()
+
+# Modify the window
+root.title("First GUI interface")
+root.config(background='grey')
+app_width = 500
+app_height = 500
+center_window(root, app_width, app_height)
+
+def my_message_1(event):
+    messagebox.showinfo(title="Event", message = "It works!")
+
+def my_message_2(event):
+    messagebox.showinfo(title="Event", message = "It works from the secound function!")
+
+root.bind('<a>',my_message_1) #This action works after click the screen
+# we have e.g. <A> if we want it to work with the letter A 
+root.bind("<a>",my_message_2, add="+") # this method help us to use the secound function after first one
+root.mainloop()
